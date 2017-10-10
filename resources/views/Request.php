0215fe4a9d96ffd50ecbae7a0c7ec24aa53202b2
Sviatko <svatoslav2104@gmail.com>
2017-10-10 19:25:43 +0300

    Initial

---
+++ resources/views/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+class Request
+{
+  public static function url()
+  {
+    return trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+
+  }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      @foreach ($tasks as $task)
+        <a href="/tasks/{{$task->id}}">
+          <li>
+            {{ $task->body }}
+          </li>
+        </a>
+      @endforeach
+    </ul>
+  </body>
+</html>
